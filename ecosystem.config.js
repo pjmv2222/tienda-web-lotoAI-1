e74b368a0a628858316699c19efa69f39b6c674d
pjmv2222 <pjmv2222@gmail.com>
2025-06-21 17:31:31 +0200

    fix(deploy): Correct backend script path in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'loto-ia-backend',
-      script: './dist/backend/src/server.js',
+      script: './dist/backend/server.js',
       instances: 1,
       autorestart: true,
       watch: false,
