d8410edd5db6890177c51b981445a748882b1f62
pjmv2222 <pjmv2222@gmail.com>
2025-07-17 16:12:18 +0200

    Eliminar llamadas conflictivas a loadPredictionsFromStorage que sobrescribían números complementarios

--- src/app/pages/elgordo/prediccion/elgordo-prediccion.component.ts
+++ src/app/pages/elgordo/prediccion/elgordo-prediccion.component.ts
@@ -97,7 +97,7 @@ export class ElgordoPrediccionComponent implements OnInit, OnDestroy {
         }
       },
       error: () => {
-        this.loadPredictionsFromStorage();
+        console.log('🔄 [DEBUG] Error al cargar desde backend, manteniendo predicciones de localStorage...');
       }
     });
   }
